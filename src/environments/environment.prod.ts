a6658d30ac54a19e4e5a8082a953dcb9788e0ca0
sharish06122007 <sharish0612@gmail.com>
2026-08-06 23:31:10 +0530

    New version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://zerohunger-production.up.railway.app/api/v1',
+  apiUrl: 'https://zerohunger-backend-production.up.railway.app/api/v1',
 };
