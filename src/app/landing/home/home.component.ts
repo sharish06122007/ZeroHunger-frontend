da76a1a4701d653478dbf5b5a65956bf4febef71
sharish06122007 <sharish0612@gmail.com>
2026-08-14 07:28:38 +0530

    new changes

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { LucideAngularModule, HeartHandshake, Utensils, PackagePlus, Users, ShieldCheck, Lock, UsersRound, Eye, Heart, Smartphone, Network, Truck, Clock, Building2, Handshake, ArrowRight, Quote, UtensilsCrossed, ArrowDown, MapPin, UserCheck, Settings } from 'lucide-angular';
 import { GradientShimmerComponent } from '../../shared/components/gradient-shimmer/gradient-shimmer.component';
-import { ScrollRevealComponent } from '../../shared/components/scroll-reveal/scroll-reveal.component';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +11,7 @@ import { ScrollRevealComponent } from '../../shared/components/scroll-reveal/scr
     CommonModule, 
     RouterLink, 
     LucideAngularModule,
-    GradientShimmerComponent,
-    ScrollRevealComponent
+    GradientShimmerComponent
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
